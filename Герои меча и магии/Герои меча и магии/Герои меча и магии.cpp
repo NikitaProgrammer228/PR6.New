75e4c458c57c6a41ece03756bd207442897186ad
vadimpro-stack <80740532+vadimpro-stack@users.noreply.github.com>
2021-03-16 10:10:43 +0300

    Update Герои меча и магии.cpp

--- Герои меча и магии/Герои меча и магии/Герои меча и магии.cpp
+++ Герои меча и магии/Герои меча и магии/Герои меча и магии.cpp
@@ -91,6 +91,8 @@ int main()
         cout << "Gold - " << archer.gold << "\n";
         cout << "Mana - " << archer.mana << "\n";
         cout << "\n";
+
+
         break;
 
     default:
@@ -99,6 +101,8 @@ int main()
     }
     goto a;
 }
+
+cout << "l";
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
 // Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
